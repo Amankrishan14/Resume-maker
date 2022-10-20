067d1c7acc6ad1db158ce14cb0dfc7ca1256de03
Amankrishan14 <krishanaman14@gmail.com>
2022-10-21 02:34:58 +0530

    yes

---
+++ src/components/landing.js
@@ -0,0 +1,17 @@
+export default function Landing() {
+    return (
+        <div className="landing" style={{ minHeight: "100vh" }}>
+
+          
+                <div className="landing-item" >
+                    <div style={{ fontSize: 60, padding: 100, fontFamily: "Courier New" }}>
+                        Resume bana le madharchod.
+                        <br/>
+                        <button className="button" type="button"> daba dijiye</button>
+                    </div>
+                    
+                </div>
+            </div>
+        
+    )
+}
---
+++ src/components/navbar.js
@@ -0,0 +1,10 @@
+function Kishans_navbar() {
+  return (
+    <div className="logo">
+      <img src="C:\Users\krish\Desktop\kissan\public\image\logo.jpg" alt="logo"/>
+    </div>
+    
+  );
+}
+
+export default Kishans_navbar;
